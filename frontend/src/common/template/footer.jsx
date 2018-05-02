c4359f1b4aed2eaf29cf5e6c61ef4ed204eb1c69
Fabio Nogueira <fabio@zeester.com.br>
2018-05-02 15:47:43 -0300

    Add backend and frontend APP My Money

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default props => (
+<footer className='main-footer'>
+  <strong>
+    Copyright &copy; 2017
+    <a href='https://fabionog.github.io/' target='_blank'>
+      Fabio Nogueira</a>.
+  </strong>
+</footer>
+)
